00f0745565eb4e5d50afe76db556d6a30a9839df
杨增文 <314850412@qq.com>
2020-04-11 16:41:56 +0800

    修改配置

--- js/public.js
+++ js/public.js
@@ -1,7 +1,7 @@
 
 // 公共配置
 
-const __ROOT__ = 'http://127.0.0.1:8085/';
+const __ROOT__ = 'http://47.244.157.136:8085/';
 const __IMG__ = 'http://jindouyun-yara.oss-cn-beijing.aliyuncs.com';
 
 var requestData = {};
