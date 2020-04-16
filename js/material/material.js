3244c74bc1d3d20bc014b8f9bdcdfda377d220fe
杨增文 <314850412@qq.com>
2020-04-16 09:05:02 +0800

    debug

--- js/material/material.js
+++ js/material/material.js
@@ -8,7 +8,7 @@ function initTable() {
         striped: true,  //表格显示条纹
         pagination: true, //启动分页
         sortName: 'id',
-        pageSize: 15,  //每页显示的记录数
+        pageSize: 10,  //每页显示的记录数
         pageNumber:1, //当前第几页
         pageList: [10, 15, 20, 25],  //记录数可选列表
         search: false,
