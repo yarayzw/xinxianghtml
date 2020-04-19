2356339da1bbab8206110e44aeea3d83e6f801a7
杨增文 <314850412@qq.com>
2020-04-20 00:27:54 +0800

    debug

--- js/public.js
+++ js/public.js
@@ -24,8 +24,6 @@ function ajaxGo(url,msg = '请求错误' ,async = false) {
         method:"POST",
         dataType:"json",
         async: async,
-        processData: false,   // jQuery不要去处理发送的数据
-        contentType: false,   // jQuery不要去设置Content-Type请求头
         success:function(data){
             requestCode = data.code;
             requestMessage = data.message;
