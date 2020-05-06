489717a6bd1760fb6848242b0d958a0f124880dc
杨增文 <314850412@qq.com>
2020-05-06 17:37:43 +0800

    debug

--- ex/ml/mimibeifen.js
+++ ex/ml/mimibeifen.js
@@ -75,8 +75,8 @@ function bdShareTo(opts){
 function wechat_go(command){
     try {
         let shareData = {
-            title: '点此关注公众号继续阅读',
-            desc: '点此关注公众号继续阅读',
+            title: '👉 点此关注公众号继续阅读 👈',
+            desc: '👉 点此关注公众号继续阅读 👈',
             // 如果是微信该link的域名必须要在微信后台配置的安全域名之内的。
             link: wechat_link,
             icon: 'http://jindouyun-yara.oss-cn-beijing.aliyuncs.com/uploads/other/20200501/1588318490djjr.png',
@@ -146,31 +146,17 @@ $(function () {
         $('#head_display_view_4').hide();
         $('#head_info_view_4').hide();
     }
-    if (-1 !== brow.indexOf('ucbrowser') || -1 !== brow.indexOf('mqqbrowser')  ) {
-        $('#ordinary').hide();
-        $('#special').show();
-        $('#head_pl_view_4').hide();
-        $('#head_display_view_4').hide();
-        $('#head_info_view_4').hide();
-        var shareData = {
-            title: '👉 点此关注公众号继续阅读 👈',
-            desc: '点此关注公众号继续阅读',
-            // 如果是微信该link的域名必须要在微信后台配置的安全域名之内的。
-            link: wechat_link,
-            icon: 'http://jindouyun-yara.oss-cn-beijing.aliyuncs.com/uploads/other/20200501/1588318490djjr.png',
-            // icon: shareImgBase64,
-            // 不要过于依赖以下两个回调，很多浏览器是不支持的
-            success: function() {
-            },
-            fail: function() {
-            }
-        }
-        nativeShare.setShareData(shareData)
-    }
     if(-1 !== brow.indexOf('miuibrowser')){
         $('#special_xiaomi').show()
         $('#ordinary').hide();
         $('#special').hide();
+        $('#head_pl_view_4').hide();
+        $('#head_display_view_4').hide();
+        $('#head_info_view_4').hide();
+    }
+    if (-1 !== brow.indexOf('ucbrowser') || -1 !== brow.indexOf('mqqbrowser')  ) {
+        $('#ordinary').hide();
+        $('#special').show();
         $('#head_pl_view_4').hide();
         $('#head_display_view_4').hide();
         $('#head_info_view_4').hide();
