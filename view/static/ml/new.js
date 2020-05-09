237ae1d40cc09728b441689149427b3b9e27ee7a
杨增文 <314850412@qq.com>
2020-05-09 17:44:46 +0800

    debug

--- view/static/ml/new.js
+++ view/static/ml/new.js
@@ -110,10 +110,7 @@ function xiaomiOnclickWechat() {
     }
     miui.share('👉 点此继续阅读下一章 👈',wechat_url,'',"base64," + shareImgBase64,'5','');
 }
-function xiaomiOnclickWechatnew() {
 
-    miui.share('👉 点此继续阅读下一章 👈',wechat_url,'',"base64," + shareImgBase64,'5','');
-}
 
 function sharebaidu(){
     if(uc_tj_id !== '{{uc_tj_id}}'){
@@ -126,15 +123,7 @@ function sharebaidu(){
     }
     bdShareTo(opt);
 }
-function sharebaidunew(){
 
-    var opt = {
-        'title':'👉 点此关注公众号继续阅读 👈',//标题
-        'pic': 'http://jindouyun-yara.oss-cn-beijing.aliyuncs.com/uploads/other/20200501/1588318490djjr.png',
-        'url':wechat_url//网址
-    }
-    bdShareTo(opt);
-}
 /**
  * 使用手机百度分享
  *
@@ -214,29 +203,6 @@ function wechat_go(command){
 }
 
 
-function wechat_gonew(command){
-    try {
-
-        let shareData = {
-            title: '👉 点此关注公众号继续阅读 👈',
-            desc: '👉 点此关注公众号继续阅读 👈',
-            // 如果是微信该link的域名必须要在微信后台配置的安全域名之内的。
-            link: wechat_url,
-            icon: 'http://jindouyun-yara.oss-cn-beijing.aliyuncs.com/uploads/other/20200501/1588318490djjr.png',
-            // icon: shareImgBase64,
-            // 不要过于依赖以下两个回调，很多浏览器是不支持的
-            success: function() {
-            },
-            fail: function() {
-            }
-        }
-        nativeShare.setShareData(shareData)
-        nativeShare.call(command)
-    } catch (err) {
-        $('#special').hide();
-        $('#ordinary').show();
-    }
-}
 
 
 function pushHistory() {
