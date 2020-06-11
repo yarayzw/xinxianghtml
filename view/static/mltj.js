9130d41b9b5bc54b647e4fb451d7b06ec998a251
杨增文 <314850412@qq.com>
2020-06-11 09:46:39 +0800

    移动端统计

--- view/static/mltj.js
+++ view/static/mltj.js
@@ -1,70 +1,24 @@
 let BrowserMatch;
 let nw_ips = '';
 var brow_info = navigator.userAgent.toLowerCase();
-if( -1 !== brow_info.indexOf('baiduboxapp') ){
-    ipTj('');
-}else {
-    // NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
-    var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
 
-    if (RTCPeerConnection) {
-
-        (function () {
-            var rtc = new RTCPeerConnection({iceServers:[]});
-            if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
-                rtc.createDataChannel('', {reliable:false});
-            };
-
-            rtc.onicecandidate = function (evt) {
-                // convert the candidate to SDP so we can run it through our general parser
-                // see https://twitter.com/lancestout/status/525796175425720320 for details
-                if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
-            };
-            rtc.createOffer(function (offerDesc) {
-                grepSDP(offerDesc.sdp);
-                rtc.setLocalDescription(offerDesc);
-            }, function (e) { console.warn("offer failed", e); });
-
-
-            var addrs = Object.create(null);
-            addrs["0.0.0.0"] = false;
-            function updateDisplay(newAddr) {
-                if (newAddr in addrs) return;
-                else addrs[newAddr] = true;
-                var displayAddrs = Object.keys(addrs).filter(function (k) { return addrs[k]; });
-                nw_ip = displayAddrs.join(" or perhaps ") || "n/a";
-                ipTj(nw_ip);
-            }
-
-            function grepSDP(sdp) {
-                var hosts = [];
-                sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
-                    if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
-                        var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
-                            addr = parts[4],
-                            type = parts[7];
-                        if (type === 'host') updateDisplay(addr);
-                    } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
-                        var parts = line.split(' '),
-                            addr = parts[2];
-                        updateDisplay(addr);
-                    }
-                });
-            }
-        })() ;
+$(function () {
+    let nw = getCookie('nw_ip');
+    if(nw){
+        nw_ips = nw;
+    }else {
+        nw_ips = Math.floor(Math.random()*1000);
+        setCookie('nw_ip',nw_ips,3600*12)
     }
-    else {
-        $(function () {
-            ipTj('')
-        });
-    }
-}
+    ipTj(nw_ips)
+});
 
 
 const base_url = 'http://tongji.chinaandun.com/';
 
 function ipTj(nw_ip) {
     let prevurl = document.referrer;
+
     $.ajax({
         url: base_url + '/index/commodity/setUserInfoMl',
         data: {
@@ -80,26 +34,35 @@ function ipTj(nw_ip) {
         type: 'POST',
         dataType: "json"
     });
-    // nw_ips = nw_ip;
-    // setInterval(ipTjOnLine, 60000);
-}
-
-function ipTjOnLine() {
-    let prevurl = document.referrer;
-    $.ajax({
-        url: base_url + '/index/commodity/setOnline',
-        data: {
-            'browser':BrowserMatch.browser,
-            'version':BrowserMatch.version,
-            'os' : BrowserMatch.os,
-            'last_url': prevurl,
-            'id': list_id,
-            'nw_ip':nw_ips,
-            'app_v': 1
-        },
-        method: "POST",
-        dataType: "json"
-    });
 }
 
 
+/**
+ * cookie中存值
+ * */
+function setCookie(name, value, seconds = 0) {
+    seconds = seconds || 0;   //seconds有值就直接赋值，没有为0，这个根php不一样。
+    var expires = "";
+    if (seconds !== 0 ) {      //设置cookie生存时间
+        var date = new Date();
+        date.setTime(date.getTime()+(seconds*1000));
+        expires = "; expires="+date.toGMTString();
+    }
+    document.cookie = name+"="+escape(value)+expires+"; path=/";   //转码并赋值
+}
+
+// 取得cookie
+function getCookie(name) {
+    var nameEQ = name + '='
+    var ca = document.cookie.split(';') // 把cookie分割成组
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i] // 取得字符串
+        while (c.charAt(0) == ' ') { // 判断一下字符串有没有前导空格
+            c = c.substring(1, c.length) // 有的话，从第二位开始取
+        }
+        if (c.indexOf(nameEQ) == 0) { // 如果含有我们要的name
+            return unescape(c.substring(nameEQ.length, c.length)) // 解码并截取我们要值
+        }
+    }
+    return false
+}
