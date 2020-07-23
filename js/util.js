7d4a8a0388c2660a25c454cc4d3ddeeea4d0c0d8
DESKTOP-N8NETBG\love <1503382884@qq.com>
2020-07-23 18:38:34 +0800

    插入内容

--- js/util.js
+++ js/util.js
@@ -25,6 +25,8 @@ modal_status = {
         });
         return serializeObj;
     };
+
+
     // 弹出层封装处理
     $.extend({
         modal: {
@@ -394,4 +396,36 @@ function getCookie(name) {
         }
     }
     return false;
+}
+
+/**
+ * 光标处插入内容
+ * @param text
+ */
+function insertContent(text) {
+    var obj = $(this)[0];
+    var range, node;
+    if (!obj.hasfocus) {
+        obj.focus();
+    }
+
+    if (document.selection && document.selection.createRange) {
+        this.focus();
+        document.selection.createRange().pasteHTML(text);
+        this.focus();
+    } else if (window.getSelection && window.getSelection().getRangeAt) {
+        range = window.getSelection().getRangeAt(0);
+        range.collapse(false);
+        node = range.createContextualFragment(text);
+        var c = node.lastChild;
+        range.insertNode(node);
+        if (c) {
+            range.setEndAfter(c);
+            range.setStartAfter(c)
+        }
+        var j = window.getSelection();
+        j.removeAllRanges();
+        j.addRange(range);
+        this.focus();
+    }
 }
