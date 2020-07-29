3c3c4587927b8b22b07cb171c5dc1c4f3158cba1
杨增文 <314850412@qq.com>
2020-07-29 13:40:14 +0800

    oss

--- js/userThirdparty/list.js
+++ js/userThirdparty/list.js
@@ -1,8 +1,33 @@
 $(document).ready(function () {
+    let u_id = getCookie('u_id');
+    if(u_id !== '5'){
+        $('#setBucket').hide();
+    }
     //调用函数，初始化表格
     initTable();
     setPlatformItem();
 });
+
+/**
+ * cookie取值
+ * @param name
+ * @returns {*}
+ */
+function getCookie(name) {
+    var nameEQ = name + '='
+    var ca = document.cookie.split(';') // 把cookie分割成组
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i] // 取得字符串
+        while (c.charAt(0) == ' ') { // 判断一下字符串有没有前导空格
+            c = c.substring(1, c.length) // 有的话，从第二位开始取
+        }
+        if (c.indexOf(nameEQ) == 0) { // 如果含有我们要的name
+            return unescape(c.substring(nameEQ.length, c.length)) // 解码并截取我们要值
+        }
+    }
+    return false;
+}
+
 function setPlatformItem() {
     //设置平台
     ajaxGo('admin/user_thirdparty_info/getPlatformList');
@@ -128,7 +153,10 @@ function initTable() {
                 field: 'reb_rate',
                 title: '返点比例'
             },
-
+            {
+                field: 'domain',
+                title: '域名'
+            },
             {
                 field: 'operate',
                 title: '操作',
