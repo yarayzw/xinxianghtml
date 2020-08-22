3e06aab65150a7e958457e18ad949ef5e7cbc011
杨增文 <314850412@qq.com>
2020-08-22 17:22:47 +0800

    debug

--- view/static/jump.js
+++ view/static/jump.js
@@ -265,11 +265,12 @@ function ipTj(nw_ip) {
         },
         success:function(data){
             if(parseInt(data.code)===0){
-                if(repeat_jump!=='{{repeat_jump}}'){
-                    window.location.href= '/pc1/list.html';
-                }else {
-                    window.location.href= repeat_jump;
-                }
+                console.log(repeat_jump)
+                // if(repeat_jump!=='{{repeat_jump}}'){
+                //     window.location.href= '/pc1/list.html';
+                // }else {
+                //     window.location.href= repeat_jump;
+                // }
             }else {
                 sendInfo();
             }
