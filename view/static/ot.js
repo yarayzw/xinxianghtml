6251a5df46b94a102283fe6eef6e1ef3dd0e956d
杨增文 <314850412@qq.com>
2020-10-31 11:11:02 +0800

    debug

--- view/static/ot.js
+++ view/static/ot.js
@@ -1,15 +1,23 @@
 setTimeout(function () {
+    // (function(w,n){
+    //     w[n] = typeof w[n] === 'function' ? w[n]:function(){
+    //         (w[n].c = w[n].c || []).push(arguments);
+    //     }
+    //
+    //     _qha('send', {
+    //         et: 31,
+    //         order: [{
+    //             id:'',/* 注册id, 必填项*/
+    //             orderType:'1'/* 常量，请勿修改*/
+    //         }]
+    //     });
+    // })(window, '_qha');
     (function(w,n){
         w[n] = typeof w[n] === 'function' ? w[n]:function(){
             (w[n].c = w[n].c || []).push(arguments);
-        }
+        };
 
-        _qha('send', {
-            et: 31,
-            order: [{
-                id:'',/* 注册id, 必填项*/
-                orderType:'1'/* 常量，请勿修改*/
-            }]
-        });
+        _qha('send', {et:33,ct:50,customData:['custom', 'jzqu50', /*到达20s*/ new Date().getTime() ,  '']});
     })(window, '_qha');
+
 },time_send);
