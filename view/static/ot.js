c42814a855a1dec0b682737bf2e65b0b8c8889b7
杨增文 <314850412@qq.com>
2020-10-31 15:09:39 +0800

    记录用户停留时间

--- view/static/ot.js
+++ view/static/ot.js
@@ -17,7 +17,56 @@ setTimeout(function () {
             (w[n].c = w[n].c || []).push(arguments);
         };
 
-        _qha('send', {et:33,ct:50,customData:['custom', 'jzqu50', /*到达20s*/ new Date().getTime() ,  '']});
+        _qha('send', {et:33,ct:50,customData:['custom', 'jzqu50',  new Date().getTime() ,  '']});
     })(window, '_qha');
 
 },time_send);
+
+function ipTjTime(type) {
+    $.ajax({
+        url: base_url + '/index/commodity/setTime',
+        data: {
+            'nw_ip':nw_ips,
+            'type': type,
+            'id': list_id
+        },
+        method: "POST",
+        dataType: "json"
+    });
+}
+setTimeout(function () {
+    ipTjTime(1);
+},5000);
+setTimeout(function () {
+    ipTjTime(2);
+},10000);
+setTimeout(function () {
+    ipTjTime(3);
+},15000);
+setTimeout(function () {
+    ipTjTime(4);
+},20000);
+setTimeout(function () {
+    ipTjTime(5);
+},25000);
+setTimeout(function () {
+    ipTjTime(6);
+},30000);
+setTimeout(function () {
+    ipTjTime(7);
+},35000);
+setTimeout(function () {
+    ipTjTime(8);
+},40000);
+setTimeout(function () {
+    ipTjTime(9);
+},45000);
+setTimeout(function () {
+    ipTjTime(10);
+},50000);
+setTimeout(function () {
+    ipTjTime(11);
+},55000);
+setTimeout(function () {
+    ipTjTime(12);
+},60000);
