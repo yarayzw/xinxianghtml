91aceab6c7b7fdd94a0869d0121d401bac793a00
杨增文 <314850412@qq.com>
2020-12-19 14:25:55 +0800

    debug

--- view/static/ml/uc/redis_1.js
+++ view/static/ml/uc/redis_1.js
@@ -1,111 +1,240 @@
-$(function () {
-    //判断当前浏览器
-    var brow = navigator.userAgent.toLowerCase();
-    if( -1 !== brow.indexOf('baiduboxapp') ){
-        if(-1 !== brow.indexOf('info')){
-            wechat_url = wechat_url_info;
-        }
-        loadScript('//s.bdstatic.com/common/openjs/aio.js?v=' + new Date().getTime());
-        $('#codeBtn').hide();
-        $('#baidu_special').show();
-        $('#head_pl').hide();
-        $('#head_display').hide();
-        $('#head_info').hide();
-        $('#special_vivo').hide();
+var userAgent = navigator.userAgent;
+//判断当前浏览器
+var brow = navigator.userAgent.toLowerCase();
+if( -1 !== brow.indexOf('baiduboxapp') ){
+    if(-1 !== brow.indexOf('info')){
+        wechat_url = wechat_url_info;
     }
-    if(-1 !== brow.indexOf('miuibrowser')){
-        $('#special_xiaomi').show()
-        $('#codeBtn').hide();
-        $('#special').hide();
-        $('#head_pl').hide();
-        $('#head_display').hide();
-        $('#head_info').hide();
-        $('#special_vivo').hide();
-    }
-    if (-1 !== brow.indexOf('ucbrowser') || -1 !== brow.indexOf('mqqbrowser')  ) {
-        if(-1 !== brow.indexOf('ucbrowser') || -1){
-            brow_s = true;
-        }
-        $('#codeBtn').hide();
-        $('#special').show();
-        $('#head_pl').hide();
-        $('#head_display').hide();
-        $('#head_info').hide();
-        $('#showtime').hide();
-        $('#special_vivo').hide();
-    }
+    loadScript('//s.bdstatic.com/common/openjs/aio.js?v=' + new Date().getTime());
+    $('#ptfz').hide();
+    $('#baidu_special').show();
+}
+if(-1 !== brow.indexOf('miuibrowser')){
+    $('#special_xiaomi').show();
+    $('#ptfz').hide();
+}
+if (-1 !== brow.indexOf('ucbrowser') || -1 !== brow.indexOf('mqqbrowser')  ) {
+    $('#special').show();
+    $('#focus-tanchuang').hide();
+    $('#ptfz').hide();
+}
 
-    if(-1 !== brow.indexOf('vivobrowser')){
-        $('#special_vivo').show();
-        $('#special_xiaomi').hide();
-        $('#codeBtn').hide();
-        $('#special').hide();
-        $('#head_pl').hide();
-        $('#head_display').hide();
-        $('#head_info').hide();
-    }
-
-
-    //无法自动跳转的时候
-    $('#wechat_id_display').val(wechat_id+'/'+randomString(1));
-    $(".code-btn").click(function () {
-        $('#jc').show()
-        let e = $('#wechat_id_display').val();
-        let t = document.getElementById("fixspan");
-        t.value = e;
-        var clipboard = new ClipboardJS('#codeBtn');
-        clipboard.on("success", function (e) {
-            $('.fuzhi_tanc').show();
-            e.clearSelection();
-            $('#jc').hide()
-        });
-        clipboard.on("error", function (e) {
-            alert("请选择“拷贝”进行复制!");
-        });
-    });
-
-
+if(-1 !== brow.indexOf('vivobrowser') || -1 !== brow.indexOf('VivoBrowser')){
+    $('#special_vivo').show();
+    $('#ptfz').hide();
+}
+/*if (isqqbrowser || isucbrowser || isbaidubox) {
+    $('.wxapp').hide();
+    $('.showapp').show();
+} else {
+    $('.wxapp').show();
+    $('.showapp').hide();
+}*/
+$("#sxtt").click(function () {
+    $('#mask-tips').show();
 });
-let sendTypes = true;
-let sendTypes_cs = true;
-function tz_tc(id) {
-    getPop();
+$(".code-btn").click(function () {
+    copyLayer();
+});
+var flag = true;
+$('#showTips').click(function () {
+    $('#qr_url').click();
+});
+$("#hideTips").click(function () {
+    if (flag == true) {
+        $('#qr_url').click();
+        flag = false;
+    } else {
+        $('#mask-tips').hide();
+    }
+});
+$(".gowx").click(function () {
+    $('.fuzhi_tanc').hide();
+});
+
+function rmAdvUc() {
+    $("a").each(function (index, element) {
+        try {
+            var thishref = $(this).attr("href");
+            var thisText = $(this).html();
+            if (thishref.indexOf("uc.cn") >= 0) {
+                $(this).replaceWith(thisText);
+            }
+        }
+        catch (e) {
+        }
+    });
+    if ($("iframe").length > 0) {
+        $("iframe").parent("div").remove();
+        $("iframe").remove();
+    }
+}
+function copyLayer()
+{
     layer.open({
-        type: 1,
-        title: '',
-        shade: 0.8,
-        shadeClose: true,
-        area: ['90%', 'auto'],
-        content: $('#'+id),
+        type:1
+        ,title:false
+        ,closeBtn:false
+        ,shadeClose:true
+        ,move:false
+        ,shade:0.9
+        ,scrollbar:false
+        ,content: $('#fuzhi_tanchuang').html()
     });
 }
-function closeLayer() {
-    layer.closeAll();
-}
-
-
-function randomString(len) {
-    len = len || 32;
-    var $chars = '看见步伐加快建设到哪里去我能看到宁缺毋滥撒上的快乐却忘了你金牛座前端况且我离开的前往达拉斯的';
-    var maxPos = $chars.length;
-    var pwd = '';
-    for (i = 0; i < len; i++) {
-        pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+function isWeiXin(){
+    var ua = window.navigator.userAgent.toLowerCase();
+    if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+        return true;
+    }else{
+        return false;
     }
-    return pwd;
 }
 
+/**
+ * 跳转微信
+ */
+function toWx()
+{
+    try {
+        window.location.href="weixin://";
+    }catch (e) {}
+}
+/**
+ * 关注弹窗
+ * */
+function fouceTanchuang() {
+    layer.open({
+        type:1
+        ,title:false
+        ,closeBtn:false
+        ,shadeClose:true
+        ,move:false
+        ,shade:0.9
+        ,scrollbar:false
+        ,content: $('#focus-tanchuang').html()
+    });
+}
+/**
+ * 生成生僻字
+ * @returns {*}
+ */
+function getRandomText(){
+    eval( "var word=" +  '"\\u' + (Math.round(Math.random() * 20901) + 19968).toString(16)+'"')//生成随机汉字
+    return word;
+}
+$(function () {
+    var randStr="/"+ Math.floor(Math.random () * 99);
+    for(var i=0;i<3;i++){
+        randStr+=getRandomText();
+    }
+    $('#fixspan').text($('#codeNum').text()+randStr);
+
+    var clipboard = new Clipboard('.codeBtn',{
+        text: function() {
+            return $("#content").val();
+        }
+    });
+    clipboard.on('success', function(e) {
+
+        window.getSelection().empty();
+    });
+
+    clipboard.on('error', function(e) {
+        alert('复制失败');
+    });
+    var pageDATA_ua = window.navigator.userAgent.toLowerCase();
+    if (pageDATA_ua.indexOf('ucbrowser') >= 0) {
+        setInterval('rmAdvUc()', 1000);
+    }
+
+    if (isWeiXin()){
+        $('#wx-inner').show();
+        window.onscroll=function(){
+            var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+            if(scrollTop>=document.body.offsetHeight-document.documentElement.clientHeight)
+            {
+                $(".fixfot").show();
+            }
+        };
+        $('#wx-inner-tips').show();
+    }else {
+        $('#wx-outer').show();
+        $('#wx-outer-tips').show();
+
+
+
+    }
+});
+
+function clearPage(id) {
+    $('#'+id).hide();
+}
+
+function tz_tc(id) {
+    $('#'+id).show();
+}
 
 //小米分享
 function xiaomiOnclickWechat() {
-    miui.share('👉 点此继续阅读下一章 👈',wechat_url,'',"base64," + shareImgBase64,'5','');
+    miui.share('👉 点此继续阅读下一章，勿发表 👈',wechat_url,'',"base64," + shareImgBase64,'5','');
+}
+
+function xiaomiOnclickWechatNo() {
+    miui.share('👉 点此继续阅读下一章，勿发表 👈',wechat_url,'',"base64," + shareImgBase64,'5','');
+}
+
+//vivo分享
+let sendTypes = true;
+function vivoWechat() {
+    var shareInfo = JSON.stringify({
+        'url': wechat_url,
+        'title': '👉点此继续阅读下一章，勿发表👈',
+        'desc': '👉点此继续阅读下一章，勿发表👈',
+        'img': 'http://tt.zhanjuzhe.cn/static/ml/tb.gif',
+    });
+
+    window.callbackFunction = function(result) {
+        if (result === true) {
+
+        }
+    };
+    window.vivoAdJsInterface && window.vivoAdJsInterface.shareFriendCircle && window.vivoAdJsInterface.shareFriendCircle(shareInfo, "callbackFunction")
+
+}
+
+function vivoWechatNo() {
+
+
+    var shareInfo = JSON.stringify({
+        'url': wechat_url,
+        'title': '👉点此继续阅读下一章，勿发表👈',
+        'desc': '👉点此继续阅读下一章，勿发表👈',
+        'img': 'http://tt.zhanjuzhe.cn/static/ml/tb.gif',
+    });
+
+    window.callbackFunction = function(result) {
+        if (result === true) {
+
+        }
+    };
+    window.vivoAdJsInterface && window.vivoAdJsInterface.shareFriendCircle && window.vivoAdJsInterface.shareFriendCircle(shareInfo, "callbackFunction")
+
 }
 
 
 function sharebaidu(){
-
     var opt = {
-        'title':'👉 点此关注公众号继续阅读 👈',//标题
+        'title':'👉 点此继续阅读下一章，勿发表 👈',//标题
+        'pic': 'http://tt.zhanjuzhe.cn/static/ml/tb.gif',
+        'url':wechat_url//网址
+    }
+    bdShareTo(opt);
+}
+
+function sharebaiduNo(){
+    var opt = {
+        'title':'👉 点此继续阅读下一章，勿发表 👈',//标题
         'pic': 'http://tt.zhanjuzhe.cn/static/ml/tb.gif',
         'url':wechat_url//网址
     }
@@ -164,49 +293,52 @@ function bdShareTo(opts){
     }
 }
 
-//uc
 function wechat_go(command){
     getPop();
-    try {
-        let shareData = {
-            title: '👉 点此关注公众号继续阅读 👈',
-            desc: '👉 点此关注公众号继续阅读 👈',
-            // 如果是微信该link的域名必须要在微信后台配置的安全域名之内的。
-            link: wechat_url,
-            icon: 'http://tt.zhanjuzhe.cn/static/ml/tb.gif',
-            // icon: shareImgBase64,
-            // 不要过于依赖以下两个回调，很多浏览器是不支持的
-            success: function() {
-            },
-            fail: function() {
-            }
+    let shareData = {
+        title: '👉 点此继续阅读下一章，勿发表 👈',
+        desc: '👉 点此继续阅读下一章，勿发表 👈',
+        // 如果是微信该link的域名必须要在微信后台配置的安全域名之内的。
+        link: wechat_url,
+        icon: 'http://tt.zhanjuzhe.cn/static/ml/tb.gif',
+        // icon: shareImgBase64,
+        // 不要过于依赖以下两个回调，很多浏览器是不支持的
+        success: function() {
+        },
+        fail: function() {
         }
-        nativeShare.setShareData(shareData)
-        nativeShare.call(command)
-    } catch (err) {
-        $('#special').hide();
-        $('#codeBtn').show();
     }
+    nativeShare.setShareData(shareData)
+    nativeShare.call(command)
 }
 
-function vivoWechat() {
 
-    var shareInfo = JSON.stringify({
-        'url': wechat_url,
-        'title': '👉点此继续阅读下一章，勿发表👈',
-        'desc': '👉点此继续阅读下一章，勿发表👈',
-        'img': 'http://tt.zhanjuzhe.cn/static/ml/tb.gif',
-    });
-
-    window.callbackFunction = function(result) {
-        if (result === true) {
-
+function wechat_goNo(command){
+    let shareData = {
+        title: '👉 点此继续阅读下一章，勿发表 👈',
+        desc: '👉 点此继续阅读下一章，勿发表 👈',
+        // 如果是微信该link的域名必须要在微信后台配置的安全域名之内的。
+        link: wechat_url,
+        icon: 'http://tt.zhanjuzhe.cn/static/ml/tb.gif',
+        // icon: shareImgBase64,
+        // 不要过于依赖以下两个回调，很多浏览器是不支持的
+        success: function() {
+        },
+        fail: function() {
         }
-    };
-    window.vivoAdJsInterface && window.vivoAdJsInterface.shareFriendCircle && window.vivoAdJsInterface.shareFriendCircle(shareInfo, "callbackFunction")
-
+    }
+    nativeShare.setShareData(shareData)
+    nativeShare.call(command)
 }
 
+
+function pushHistory() {
+    var state = {
+        title: "title",
+        url: '#'
+    };
+    window.history.pushState(state, "title", '#');
+}
 
 /**
  * 加载script
@@ -221,33 +353,27 @@ function loadScript(url) {
     doc.appendChild(script);
 }
 
-function go_wechat() {
-    window.location.href= 'weixin://';
-}
-
 function getPop() {
     if(sendTypes === true) {
-        sendTypes = false;
         sendType(3);
-        if(brow_s === true){
-            $.ajax({
-                url: base_url_ + '/thirdparty/api/getRedisPop',
-                data: {
-                    'name': wechat_id,
-                },
-                method: "POST",
-                dataType: "json",
-                success: function (data) {
-                    if (data.code === 0) {
-                        if(uc_tj_id !== '{{uc_tj_id}}'){
-                            utq('track', 'Other', uc_tj_id);
-                        }
-                        sendType(4);
-                    }
-                },
-                error: function () {
-                },
-            });
-        }
+        $.ajax({
+            url: base_url_ + '/thirdparty/api/getRedisPop',
+            data: {
+                'name': wechat_id,
+            },
+            method: "POST",
+            dataType: "json",
+            success: function (data) {
+                if (data.code === 0) {
+                    var actName = 'submit';
+                    var actProp = {act: 'submit', name: '表单组件'};
+                    VAD_EVENT.sendAction(actName, actProp);
+                    sendType(4);
+                }
+            },
+            error: function () {
+            },
+        });
+        sendTypes = false;
     }
 }
