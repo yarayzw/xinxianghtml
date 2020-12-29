65b3171c2e7d5439ea952a742e774bf41ef5622a
杨增文 <314850412@qq.com>
2020-12-29 16:42:54 +0800

    sina 移动端

---
+++ view/static/ml/sina/address.js
@@ -0,0 +1,55 @@
+const   base_url_ = 'http://tongji.zhanjuzhe.cn/';
+
+
+$(document).ready(function() {
+    $('#head_img_div').hide();
+    getAddress();
+
+    let myDate = new Date();
+    let qr_code_s = qr_code.split('@@@');
+    let update_at_c = (new Date(update_at)).getTime() / 1000;
+
+    let year=myDate.getFullYear();        //获取当前年
+    let month=myDate.getMonth()+1;   //获取当前月
+    let date=myDate.getDate();            //获取当前日
+
+
+    let h=myDate.getHours();              //获取当前小时数(0-23)
+    let m=myDate.getMinutes();          //获取当前分钟数(0-59)
+    let s=myDate.getSeconds();
+    let now = year+'-'+getNow(month)+"-"+getNow(date);
+    let now_c = (new Date(now)).getTime() / 1000;
+
+    let time = (now_c - update_at_c) / 24/3600;
+
+    if (time >= qr_code_s.length) {
+        time = time % qr_code_s.length;
+    }
+    $('#qr_code_bottom').attr('src',qr_code_s[time]);
+});
+function getNow(s) {
+    return s < 10 ? '0' + s: s;
+}
+
+function getAddress() {
+    $.ajax({
+        url: base_url_ + '/thirdparty/api/getAsm',
+        data: {
+            'url': document.referrer
+        },
+        method: "POST",
+        dataType: "json",
+        success: function (data) {
+            let head_img = img_url.split('@@@');
+            if (data.code === 0) {
+                $('#head_img').attr('src',head_img[0]);
+                $('#qx_span').show();
+            }else {
+                $('#head_img').attr('src',head_img[1]);
+            }
+            $('#head_img').show();
+        },
+        error: function () {
+        },
+    });
+}
