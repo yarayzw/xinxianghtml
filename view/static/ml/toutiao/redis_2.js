2594d0669e759bbc39ebfeb9470f7f8529509abe
杨增文 <314850412@qq.com>
2021-04-26 19:35:47 +0800

    debug

--- view/static/ml/toutiao/redis_2.js
+++ view/static/ml/toutiao/redis_2.js
@@ -1,46 +1,8 @@
 //小程序跳转
 function xcx() {
-    getPop();
     location.href = xcx_url;
 }
 
 $(document).ready(function() {
     xcx();
 });
-let sendTypes = true;
-
-/**
- * 加载script
- *
- * @inner
- * @param {string} url 加载的js地址
- */
-function loadScript(url) {
-    var script = document.createElement('script');
-    var doc = document.getElementsByTagName('body')[0];
-    script.setAttribute('src', url);
-    doc.appendChild(script);
-}
-
-function getPop() {
-    if(sendTypes === true) {
-        sendType(3);
-        $.ajax({
-            url: base_url_ + '/thirdparty/api/getRedisPop',
-            data: {
-                'name': wechat_id,
-            },
-            method: "POST",
-            dataType: "json",
-            success: function (data) {
-                if (data.code === 0) {
-                    meteor.track("form", {convert_id: uc_tj_id});
-                    sendType(4);
-                }
-            },
-            error: function () {
-            },
-        });
-        sendTypes = false;
-    }
-}
