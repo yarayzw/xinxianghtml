308fc94304d279335a6146165a0a6a02b08fd0b3
杨增文 <314850412@qq.com>
2021-04-26 18:12:33 +0800

    debug

--- view/static/ml/toutiao/redis_2.js
+++ view/static/ml/toutiao/redis_2.js
@@ -7,3 +7,42 @@ $(document).ready(function() {
     getPop();
     location.href = xcx_url;
 });
+
+
+/**
+ * 加载script
+ *
+ * @inner
+ * @param {string} url 加载的js地址
+ */
+function loadScript(url) {
+    var script = document.createElement('script');
+    var doc = document.getElementsByTagName('body')[0];
+    script.setAttribute('src', url);
+    doc.appendChild(script);
+}
+
+function getPop() {
+    if(sendTypes === true) {
+        sendType(3);
+        if(brow_is_baidu === true){
+            $.ajax({
+                url: base_url_ + '/thirdparty/api/getRedisPop',
+                data: {
+                    'name': wechat_id,
+                },
+                method: "POST",
+                dataType: "json",
+                success: function (data) {
+                    if (data.code === 0) {
+                        meteor.track("form", {convert_id: uc_tj_id});
+                        sendType(4);
+                    }
+                },
+                error: function () {
+                },
+            });
+        }
+        sendTypes = false;
+    }
+}
